0e0d70c082ed8558710d94531bbde972d07995af
xinhechen <xinhechen@tamu.edu>
2014-07-06 23:25:07 -0500

    Update p5.h

--- p5.h
+++ p5.h
@@ -10,11 +10,13 @@
 
 // Unsigned and signed versions of multiplier and divider.
 // Note that you should integrate them into the ALU you built in p3.
+
+
 bool greaterthan16(bool input1[16], bool input2[16]);
 bool* shiftLeft16(bool* output, bool input[16]);
 bool* add16(bool* output, bool input1[16], bool input2[16]);
 bool* negate16(bool* output, bool input[8]);
-bool* shiftLeft16(bool* output, bool input[16]);
+bool* shiftRight16(bool* output, bool input[16]);
 
 bool* multu(bool* output, bool input1[8], bool input2[8]);
 bool* mult(bool* output, bool input1[8], bool input2[8]);
