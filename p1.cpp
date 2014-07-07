2f49ce01214a09b409583ef243aa6fcb300db682
xinhechen <xinhechen@tamu.edu>
2014-07-06 23:15:48 -0500

    Update p1.cpp

--- p1.cpp
+++ p1.cpp
@@ -3,7 +3,7 @@
 #include "basic_functions.h"
 
 using namespace std;
-     bool XOR(bool a, bool b)
+bool XOR(bool a, bool b)
 {
     bool na=Not(a);
     bool nb=Not(b);
@@ -11,39 +11,41 @@ using namespace std;
     bool nba=And(nb,a);
     bool ans=Or(nab,nba);
     return ans;
-}   
-    bool Mux(bool operation, bool output_if_true, bool output_if_false)
-    {
-        bool no=Not(operation);
-        bool oa=And(operation,output_if_true);
-        bool nob=And(no,output_if_false);
-        bool ans=Or(oa,nob);
-        return ans;
-    }
-    
-    bool Sum(bool a, bool b)
-    {
-        return XOR(a,b);
-    }
-    
-    bool Sum(bool a, bool b, bool c)
-    {
-        return XOR(a,XOR(b,c));
-    }
-    
-    bool CarryOut(bool a, bool b, bool c)
-    {
-        bool ab=And(a,b);
-        bool ac=And(a,c);
-        bool bc=And(b,c);
-        bool ans=Or(ab,Or(ac,bc));
-        return ans;
-    }
-    
-    bool Equal(bool a, bool b)
-    {
-        return Not(XOR(a,b));
-    }
+}
+bool Mux(bool operation, bool output_if_true, bool output_if_false)
+{
+    bool no=Not(operation);
+    bool oa=And(operation,output_if_true);
+    bool nob=And(no,output_if_false);
+    bool ans=Or(oa,nob);
+    return ans;
+}
+
+bool Sum(bool a, bool b)
+{
+    return XOR(a,b);
+}
+
+bool Sum(bool a, bool b, bool c)
+{
+    return XOR(a,XOR(b,c));
+}
+
+bool CarryOut(bool a, bool b, bool c)
+{
+    bool ab=And(a,b);
+    bool ac=And(a,c);
+    bool bc=And(b,c);
+    bool ans=Or(ab,Or(ac,bc));
+    return ans;
+}
+
+bool Equal(bool a, bool b)
+{
+    return Not(XOR(a,b));
+}
+
+
     
 
     
